4488a267d15010475f2f1c06188f36a7c5433878
More-F <andresfel38@gmail.com>
2025-08-14 15:40:13 -0500

    Agregacion de la pagina catlogo y correccion script navar

---
+++ JS/navar.js
@@ -0,0 +1,26 @@
+import { animate, stagger, text } from 'https://cdn.jsdelivr.net/npm/animejs/+esm';
+
+document.addEventListener("DOMContentLoaded", () => {
+    
+    const { words } = text.split('.titulo-nav',  {
+    words: { wrap: 'clip' },
+})
+
+animate(words, {
+    y: [
+        { to: ['100%', '0%'] },
+        { to: '-100%', delay: 1750, ease: 'in(3)' }
+    ],
+    duration: 1750,
+    ease: 'out(3)',
+    delay: stagger(100),
+    loop: true,
+});
+
+const menuToggle = document.getElementById("menu-toggle");
+    const navList = document.querySelector(".nav-list");
+
+    menuToggle.addEventListener("click", () => {
+        navList.classList.toggle("active");
+    });
+});
